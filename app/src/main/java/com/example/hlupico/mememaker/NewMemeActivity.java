d500f3ce5339352002ab89b797c42398f27ec891
chichtech9 <chicktechies+9@gmail.com>
2017-12-10 08:11:37 -0800

    my changes

---
+++ app/src/main/java/com/example/hlupico/mememaker/NewMemeActivity.java
@@ -0,0 +1,13 @@
+package com.example.hlupico.mememaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewMemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_meme2);
+    }
+}
